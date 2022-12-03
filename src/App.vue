1ce3c9a017c0c9ce07c5d74130cae1075b076923
noemipintaldi <noemipintaldi@gmail.com>
2022-12-03 16:08:33 +0100

    vue main structure completed

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <LoginComp/>
+  </div>
+</template>
+
+<script>
+import LoginComp from './components/LoginComp.vue'
+import "bootstrap"
+
+export default {
+  name: 'App',
+  components: {
+    LoginComp
+  }
+}
+</script>
+
+<style lang="scss">
+@import "bootstrap/dist/css/bootstrap.min.css"; 
+
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+</style>
