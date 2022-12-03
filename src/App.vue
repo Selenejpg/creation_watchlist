f1908ce626acfe3d1d6d05cba86b97d24a08b997
noemipintaldi <noemipintaldi@gmail.com>
2022-12-03 20:41:47 +0100

    fixes to login page

--- src/App.vue
+++ src/App.vue
@@ -2,19 +2,22 @@
   <div id="app">
     <HeaderComp/>
     <LoginComp/>
+    <FooterComp/>
   </div>
 </template>
 
 <script>
-import LoginComp from './components/LoginComp.vue'
 import HeaderComp from './components/HeaderComp.vue'
+import LoginComp from './components/LoginComp.vue'
+import FooterComp from './components/FooterComp.vue'
 import "bootstrap"
 
 export default {
   name: 'App',
   components: {
     HeaderComp,
-    LoginComp
+    LoginComp,
+    FooterComp
   }
 }
 </script>
---
+++ src/components/FooterComp.vue
@@ -0,0 +1,34 @@
+<template>
+    <footer>
+        <div>
+            <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ" target="_blank">Non hai un account? Registrati qui.</a>
+        </div>
+    </footer>
+</template>
+
+<script>
+    export default {
+    //Cambiare il nome con quello del componente creato
+    name: 'FooterComp',
+    }
+</script>
+
+<style scoped lang="scss">
+@import '../sass/breakpoint.scss';
+@import '../sass/variables.scss';
+
+    footer{
+        margin-top: 20px;
+        margin-bottom: 50px;
+        text-align: center;
+        font-size: 14px;
+        @include tablet-min{
+            margin-top: 40px;
+            font-size: 16px;
+        }
+        a{
+            text-decoration: none;
+            color: gray;
+        }
+    }
+</style>
